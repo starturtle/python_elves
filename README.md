586362be3cd8a616a56b11fdc5374390809e7f7c
starturtle <27267274+starturtle@users.noreply.github.com>
2025-07-04 13:45:36 +0200

    improvements
    detailed documentation, proper plural handling, error for negative values, configurable stack size, rename from blocks to items. added category-wise pretty printer function and specifyable chest/container size.

--- README.md
+++ README.md
@@ -54,6 +54,21 @@ In order to use one query inside another, use the sub-query's name in brackets i
 
 ## How Many Do I Need?
 The minecraft/hmdin script is basically a counting formatter that can help you keep track of large item quantities in Minecraft.
-For the time being, it supports items that stack at 64 pieces, and expects single chests with 27 slots. 
+By default, it will print a human-readable listing of all nonzero "units of quantity" necessary to amass the specified amount of items.
 
-Input: the amount of items to count. Output: `<number of chests>c, <number of additional stacks>s, <number of additional items>b`.
+You can specify a nonstandard stack size (e.g. 16 for eggs or similar) using `--stack-size`.
+
+    python hmdin.py 256 --stack_size=16
+	> 256 items are 16 stacks
+	
+	python hmdin.py 256
+	> 255 items are 3 stacks and 63 items
+	
+	python hmdin.py 2000
+	> 2000 items are 1 chest, 4 stacks and 16 items
+	
+The actual helper/split functions are:
+
+	hmdin(count, stack_size=64, container_size=27) # will return the tuple of <chests>, <stacks>, <items>
+	pretty_print(chests, stacks, items) # will format the list of items as human readable (without prefix), e.g. "3 chests and 14 items"
+	csv_print(chests, stacks, items, separator=",") #will format the list of all three items for CSV output, e.g. comma-separated
--- minecraft/hmdin.py
+++ minecraft/hmdin.py
@@ -1,32 +1,40 @@
 import argparse
+import sys
 
-def hmdin(count):
-  BPS = 64
-  SPC = 27
-  b = count % BPS
-  s_raw = count // BPS
-  s = s_raw % SPC
-  c = s_raw // SPC
-  return c, s, b
+def hmdin(count, stack_size=64, container_size=27):
+  i = count % stack_size
+  s_raw = count // stack_size
+  s = s_raw % container_size
+  c = s_raw // container_size
+  return c, s, i
 
-def pretty_print(chests, stacks, blocks):
+def pretty_print_kind(count, name):
+  return f"{count} {name}{'' if count == 1 else 's'}"
+
+def pretty_print(chests, stacks, items):
   components = []
   if chests > 0:
-    components.append(f"{chests} chests")
+    components.append(pretty_print_kind(chests, "chest"))
   if stacks > 0:
-    components.append(f"{stacks} stacks")
-  if blocks > 0 or len(components) == 0:
-    components.append(f"{blocks} blocks")
+    components.append(pretty_print_kind(stacks, "stack"))
+  if items > 0 or len(components) == 0:
+    components.append(pretty_print_kind(items, "item"))
   if len(components) == 1:
     return components[0]
   return ", ".join(components[:-1]) + " and " + components[-1]
   
-def csv_print(chests, stacks, blocks, separator=","):
-  return separator.join(chests, stacks, blocks)
+def csv_print(chests, stacks, items, separator=","):
+  return separator.join(chests, stacks, items)
   
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
-  parser.add_argument("-count", help="Number of blocks to pretty-print", type=int)
+  parser.add_argument("count", help="Number of items to evaluate", type=int)
+  parser.add_argument("--stack_size", help="Stack size of the respective item", type=int, default=64)
+  parser.add_argument("--as_csv", action="store_true")
   args = parser.parse_args()
-  print(f"{args.count} blocks are {pretty_print(*hmdin(args.count))}.")
+  printer = csv_print if args.as_csv else pretty_print
+  if args.count < 0:
+    print(f"-{pretty_print_kind(-args.count, 'item')} {'are' if args.count != 1 else 'is'} imaginary.")
+    sys.exit(1)
+  print(f"{pretty_print_kind(args.count, 'item')} {'are' if args.count != 1 else 'is'} {printer(*hmdin(args.count, args.stack_size))}.")
   
