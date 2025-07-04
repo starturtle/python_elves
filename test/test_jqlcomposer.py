d46841fe05d36c8cedb3fb978b78ee97f6815593
starturtle <27267274+starturtle@users.noreply.github.com>
2025-07-04 17:47:30 +0200

    first-shot test for jqlcomposer
    first try with first file - there needs to be much more

---
+++ test/test_jqlcomposer.py
@@ -0,0 +1,23 @@
+import jqlcomposer.jqlcomposer
+import subprocess
+import filecmp
+
+TEST_TERMS=["summer"]
+
+test_jqlcomposer_main_asfile():
+  for i in xrange(len(TEST_TERMS)):
+    idx = i+1
+    with open(f"testout{idx}.txt", "w") as outfile
+      subprocess.run(["python", "jqlcomposer.py", TEST_TERMS[i], "--filename", f"test{idx}.json"], stdout=outfile)
+    assert 0 == filecmp.cmp(f"testout{idx}.txt", f"expect{idx}.txt")
+  
+test_jqlcomposer_main_astext():
+  for i in xrange(len(TEST_TERMS)):
+    idx = i+1
+    json_contents = ""
+    with open(f"test{idx}.json", "r") as infile:
+      json_contents = infile.read()
+    with open(f"testout{idx}.txt", "w") as outfile
+      subprocess.run(["python", "jqlcomposer.py", TEST_TERMS[i], f'"{json_contents}"'], stdout=outfile)
+    assert 0 == filecmp.cmp(f"testout{idx}.txt", f"expect{idx}.txt")
+  
